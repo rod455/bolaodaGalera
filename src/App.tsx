08e0df6ffb80d611972157ab47157a8af175f7ec
rod455 <rodrigomoraessilva455@gmail.com>
2026-02-12 19:25:48 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "@/contexts/AuthContext";
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
 import CriarBolao from "./pages/CriarBolao";
 import EntrarBolao from "./pages/EntrarBolao";
+import AoVivo from "./pages/AoVivo";
 import Perfil from "./pages/Perfil";
 import BolaoPage from "./pages/BolaoPage";
 import Palpites from "./pages/Palpites";
-import Planos from "./pages/Planos";
 import NotFound from "./pages/NotFound";
 import AppLayout from "./components/layout/AppLayout";
-import ProtectedRoute from "./components/layout/ProtectedRoute";
 
 const queryClient = new QueryClient();
 
@@ -23,30 +21,22 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/auth" replace />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route path="/home" element={<Home />} />
-              <Route path="/criar" element={<CriarBolao />} />
-              <Route path="/entrar" element={<EntrarBolao />} />
-              <Route path="/perfil" element={<Perfil />} />
-              <Route path="/bolao/:id" element={<BolaoPage />} />
-              <Route path="/bolao/:id/palpites" element={<Palpites />} />
-            <Route path="/planos" element={<Planos />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/auth" replace />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route element={<AppLayout />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/criar" element={<CriarBolao />} />
+            <Route path="/entrar" element={<EntrarBolao />} />
+            <Route path="/ao-vivo" element={<AoVivo />} />
+            <Route path="/perfil" element={<Perfil />} />
+            <Route path="/bolao/:id" element={<BolaoPage />} />
+            <Route path="/bolao/:id/palpites" element={<Palpites />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
