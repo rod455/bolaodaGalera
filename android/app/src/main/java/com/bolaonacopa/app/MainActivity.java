a8358018244efebef2b917d595a006b8c46cc639
rod455 <rodrigomoraessilva455@gmail.com>
2026-02-15 12:24:59 -0300

    Update MainActivity.java

--- android/app/src/main/java/com/bolaonacopa/app/MainActivity.java
+++ android/app/src/main/java/com/bolaonacopa/app/MainActivity.java
@@ -1,5 +1,28 @@
 package com.bolaonacopa.app;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                "bolao_default",
+                "Bolão na Copa",
+                NotificationManager.IMPORTANCE_DEFAULT
+            );
+            channel.setDescription("Notificações do Bolão na Copa");
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+}
