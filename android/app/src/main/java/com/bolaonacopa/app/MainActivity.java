36a668f11d31483cf8811a0dfaebd09fc072e6e6
rod455 <rodrigomoraessilva455@gmail.com>
2026-02-21 09:45:05 -0300

    Add files via upload

--- android/app/src/main/java/com/bolaonacopa/app/MainActivity.java
+++ android/app/src/main/java/com/bolaonacopa/app/MainActivity.java
@@ -5,12 +5,10 @@ import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        registerPlugin(GoogleAuth.class);
         super.onCreate(savedInstanceState);
         createNotificationChannel();
     }
